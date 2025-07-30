5315932396cfec810da65e88f48f7d12945b70e4
Ángel González <angel.gonzalez@radmas.com>
2025-07-30 08:11:16 +0200

    Initial Commit

---
+++ Umas/UmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmasApp.swift
+//  Umas
+//
+//  Created by Ángel González on 30/7/25.
+//
+
+import SwiftUI
+
+@main
+struct UmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
